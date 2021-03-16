907855a16855540980137d73320f9d9d8d3af3cb
Jonathan Schwartz <>
2021-03-16 16:35:33 -0400

    Got stepper to run consistently at ~150 RPM

--- stepper_motor/stepper_motor.ino
+++ stepper_motor/stepper_motor.ino
@@ -3,12 +3,13 @@
 #define dirPin 8
 
 #define stepAngle 1.8
-#define stepIncrement 5   // may need to change this
+
+int stepIncrement = 6; //initial step increment, can be changed in accel loop  
 
 float deg = 0;
-int pos = 0;
+long pos = 0;
 int val = 0;
-int desPos = 8000; // 800 ticks per revolution
+long desPos = 144000; //8050; // 800 ticks per revolution at slow speed
 
 const int stepsPerRev = 800;
 
@@ -16,21 +17,31 @@ Stepper stepper(stepsPerRev, stepPin, dirPin);
 
 void setup() {
   Serial.begin(9600);
-  stepper.setSpeed(1000); //speed in RPM
+  stepper.setSpeed(500); //speed in RPM
+
+  Serial.println(desPos);
 }
 
 void loop() {
+  //accelerate
+//  if(pos/400 > stepIncrement-1 && stepIncrement < 6){
+//    stepIncrement++;
+//    Serial.println(pos);
+//    Serial.println(stepIncrement);
+//  }
+  
   if (desPos > pos + stepIncrement - 1) {
     stepper.step(stepIncrement);
     pos = pos + stepIncrement;
   } else if (desPos < pos - stepIncrement - 1) {
     stepper.step(-stepIncrement);
     pos = pos - stepIncrement;
+  } else{
+    Serial.println(pos);
+    Serial.println(desPos);
   }
-
-//  deg = posToDegree(pos);
-//  Serial.print("stepper ");
-//  Serial.println(deg);
-  Serial.println(pos);
-//  delay(50);
+  //  deg = posToDegree(pos);
+  //  Serial.print("stepper ");
+  //  Serial.println(deg);
+  delayMicroseconds(1600);
 }
