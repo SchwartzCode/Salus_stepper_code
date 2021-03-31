1c741ad1e45347c923edcb2e761c8fa9b10a6645
Jonathan Schwartz <>
2021-03-30 21:48:57 -0400

    Fixed sign issue

--- stepper_motor_position_input/stepper_motor_position_input.ino
+++ stepper_motor_position_input/stepper_motor_position_input.ino
@@ -9,9 +9,10 @@ int stepIncrement = 6; //initial step increment, can be changed in accel loop
 float deg = 0;
 long pos = 0;
 int val = 0;
-long desPos = 0; //8050; // 800 ticks per revolution at slow speed
+long desPos = pos; //8050; // 800 ticks per revolution at slow speed
 
-long maxPos = 130000;
+long maxPos = 0;
+long minPos = -130000;
 
 const int stepsPerRev = 800;
 
@@ -21,22 +22,22 @@ void setup() {
   Serial.begin(9600);
   stepper.setSpeed(500); //speed in RPM
 
-  Serial.println(desPos);
+  Serial.println("Enter desired change in position in mm:");
 }
 
 void loop() {
   if(Serial.available()){
     String user_input = Serial.readString();
     // convert desired change in pos from mm to stepper ticks
-    long changeInPos = stepsPerRev*user_input.toFloat()/5; 
+    long changeInPos = -stepsPerRev*user_input.toFloat()/5; 
     if(changeInPos != 0)
 //      Serial.println(changeInPos);
 //      Serial.println(desPos);
       desPos += changeInPos;
       if(desPos > maxPos)
         desPos = maxPos;
-      else if (desPos<0)
-        desPos = 0;
+      else if (desPos<minPos)
+        desPos = minPos;
 //      Serial.println(desPos);
 //      Serial.println("\n\n");
   }
